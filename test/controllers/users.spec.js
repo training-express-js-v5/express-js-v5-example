7b748c40326abf748f0578de9f62f974e660f164
julian molina <molina.bohemio@gmail.com>
2019-12-05 12:37:38 -0300

    changes for review

--- test/controllers/users.spec.js
+++ test/controllers/users.spec.js
@@ -1,24 +1,26 @@
-const { user: getFakeUser, email, name, lastName, password } = require('../helpers/faker');
 const { successHash, failHash, successCompare, failCompare } = require('../mocks/bcrypt');
 const User = require('../../app/models').users;
 const { getResponse, truncateDatabase } = require('../helpers/app');
-const { create } = require('../factory/users');
+const { create, build } = require('../factory/users');
 const { successSignIn } = require('../mocks/jwt');
 
 describe('Users controllers', () => {
   beforeAll(() => truncateDatabase());
   describe('POST users', () => {
+    let user = {};
+    beforeAll(async () => {
+      user = await build({ password: '12345678Aa' });
+    });
     describe('Successful cases', () => {
       describe('Create user successfully', () => {
         let response = {};
         let newUser = {};
-        const user = getFakeUser();
         beforeAll(async () => {
           successHash('I am a password encrypted');
           response = await getResponse({
             endpoint: '/users',
             method: 'post',
-            body: { ...user, email: 'fake@domain.com' }
+            body: { ...user.dataValues, email: 'fake@domain.com' }
           });
           newUser = await User.findOne();
         });
@@ -33,66 +35,69 @@ describe('Users controllers', () => {
           expect(newUser.password).toBe('I am a password encrypted');
         });
       });
-      describe('Failure cases', () => {
-        describe.each([
-          { email: email(), password: password(), name: name() },
-          { lastName: lastName(), email: email(), password: password() },
-          { name: name(), lastName: lastName(), email: email() },
-          { password: password(), name: name(), lastName: lastName() },
-          {}
-        ])('Failure for Invalid body schema sent in the request: %p', body => {
-          let error = {};
-          beforeAll(async () => {
-            error = await getResponse({
-              body,
-              endpoint: '/users',
-              method: 'post'
-            });
-          });
-          it('The error must be schema error', () => {
-            expect(error.body.internal_code).toBe('schema_error');
-          });
-          it('The statusCode must be 422', () => {
-            expect(error.statusCode).toBe(422);
+    });
+    describe('Failure cases users', () => {
+      const { email, lastName, password, name } = user;
+      describe.each([
+        { email, password, name },
+        { lastName, email, password },
+        { name, lastName, email },
+        { password, name, lastName },
+        {}
+      ])('Failure for Invalid body schema sent in the request: %p', body => {
+        let error = {};
+        beforeAll(async () => {
+          error = await getResponse({
+            body,
+            endpoint: '/users',
+            method: 'post'
           });
         });
-        describe('Failure because bcrypt is down', () => {
-          let response = {};
-          const user = getFakeUser();
-          beforeAll(async () => {
-            failHash('I am down');
-            response = await getResponse({
-              endpoint: '/users',
-              method: 'post',
-              body: { ...user, email: 'fake@domain.com' }
-            });
+        it('The error must be schema error', () => {
+          expect(error.body.internal_code).toBe('schema_error');
+        });
+        it('The statusCode must be 422', () => {
+          expect(error.statusCode).toBe(422);
+        });
+      });
+      describe('Failure because bcrypt is down', () => {
+        let response = {};
+        beforeAll(async () => {
+          failHash('I am down');
+          response = await getResponse({
+            endpoint: '/users',
+            method: 'post',
+            body: { ...user.dataValues, email: 'fake@domain.com' }
           });
-          it('The response status code must be 500', () => {
-            expect(response.statusCode).toBe(500);
-          });
-          it('The response body message must be "Error hashing password"', () => {
-            expect(response.body.message).toBe('Error hashing password');
-          });
-          it('The response body internalCode must be "hash_error"', () => {
-            expect(response.body.internal_code).toBe('hash_error');
-          });
+        });
+        it('The response status code must be 500', () => {
+          expect(response.statusCode).toBe(500);
+        });
+        it('The response body message must be "Error hashing password"', () => {
+          expect(response.body.message).toBe('Error hashing password');
+        });
+        it('The response body internalCode must be "hash_error"', () => {
+          expect(response.body.internal_code).toBe('hash_error');
         });
       });
     });
   });
   describe(' POST users/login', () => {
+    let user = {};
+    beforeAll(async () => {
+      user = await build({ password: '12345678Aa' });
+    });
     describe('Successful cases', () => {
       describe('Login successful', () => {
         let response = {};
-        const user = getFakeUser();
         beforeAll(async () => {
-          await create({ ...user, email: 'fake@domain.com' });
+          await create({ email: 'fake@domain.com', password: user.dataValues.password });
           successSignIn('I am a token');
           successCompare(true);
           response = await getResponse({
             endpoint: '/users/login',
             method: 'post',
-            body: { email: 'fake@domain.com', password: user.password }
+            body: { email: 'fake@domain.com', password: user.dataValues.password }
           });
         });
         afterAll(() => truncateDatabase());
@@ -105,7 +110,7 @@ describe('Users controllers', () => {
       });
     });
     describe('Failure cases', () => {
-      describe.each([{ email: email() }, { password: password() }, {}])(
+      describe.each([{ email: 'fake@gmail.com' }, { password: '12345678Aa' }, {}])(
         'Failure for Invalid body schema sent in the request: %p',
         body => {
           let error = {};
@@ -126,14 +131,13 @@ describe('Users controllers', () => {
       );
       describe('Failure for invalid password', () => {
         let error = {};
-        const user = getFakeUser();
         beforeAll(async () => {
-          await create({ ...user, email: 'fake@domain.com' });
+          await create({ email: 'fake@domain.com', password: user.dataValues.password });
           successCompare(false);
           error = await getResponse({
             endpoint: '/users/login',
             method: 'post',
-            body: { email: 'fake@domain.com', password: user.password }
+            body: { email: 'fake@domain.com', password: user.dataValues.password }
           });
         });
         afterAll(() => truncateDatabase());
@@ -147,16 +151,15 @@ describe('Users controllers', () => {
           expect(error.body.message).toBe('Username or password invalid');
         });
       });
-      describe('Failure because bcrypt is down', () => {
+      describe('Failure because bcrypt is down login', () => {
         let error = {};
-        const user = getFakeUser();
         beforeAll(async () => {
-          await create({ ...user, email: 'fake@domain.com' });
+          await create({ email: 'fake@domain.com', password: user.dataValues.password });
           failCompare('I am down');
           error = await getResponse({
             endpoint: '/users/login',
             method: 'post',
-            body: { email: 'fake@domain.com', password: user.password }
+            body: { email: 'fake@domain.com', password: user.dataValues.password }
           });
         });
         afterAll(() => truncateDatabase());
