ee85cec1197abc448653a72ac99392df6457851e
andreaceltich <50246314+andreaceltich@users.noreply.github.com>
2019-12-05 14:57:26 -0300

    Revert "Get weets"
    This reverts commit 760681e397b5181bd9980ae7b1845444d99e06e2.

--- app/controllers/weets.js
+++ app/controllers/weets.js
@@ -1,14 +1,9 @@
 const { getRandomFact } = require('../services/numbers');
-const { createWeet, getAllWeets } = require('../services/weets');
+const { createWeet } = require('../services/weets');
 
 exports.create = ({ user }, res, next) =>
   getRandomFact()
     .then(numberFact =>
       createWeet({ content: numberFact.slice(0, 140), userId: user.id }).then(() => res.status(201).end())
     )
-    .catch(next);
-
-exports.getAll = (req, res, next) =>
-  getAllWeets(req.query.limit, req.query.page)
-    .then(weets => res.send(weets))
     .catch(next);
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ const users = require('./controllers/users');
 const weets = require('./controllers/weets');
 const schemaValidator = require('./middlewares/schemas_validator');
 const { userSignUpSchema, userLogInSchema } = require('./schemas/users');
-const { paginationSchema } = require('./schemas/pagination');
 const { checkUser, authenticate } = require('./middlewares/authentication');
 
 exports.init = app => {
@@ -11,5 +10,4 @@ exports.init = app => {
   app.post('/users', [schemaValidator(userSignUpSchema)], users.signUp);
   app.post('/users/login', [schemaValidator(userLogInSchema), checkUser], users.logIn);
   app.post('/weets', authenticate, weets.create);
-  app.get('/weets', [authenticate, schemaValidator(paginationSchema)], weets.getAll);
 };
